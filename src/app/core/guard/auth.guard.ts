49035cc68cdbb32f9dd20899c83c219a8a16ffd4
kbburgos <kbburgos@espol.edu.ec>
2020-08-17 01:51:01 -0500

    UPDATE validacion campos login y validacion rol (comentado)

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -1,26 +1,27 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from "@angular/router";
+import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { isNullOrUndefined } from 'util';
-import { AuthService} from '../services/auth/auth.service';
+import { isNullOrUndefined } from "util";
+import { AuthService } from "../services/auth/auth.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
-
-  
-  constructor(
-    private authService: AuthService, private router: Router
-  ) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
     if (this.authService.isLoggedIn()) {
       return true;
     }
-    this.router.navigate(["login"]);
+    this.router.navigateByUrl("login");
     return false;
   }
-  
 }
